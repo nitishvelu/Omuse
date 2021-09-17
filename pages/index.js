9f237a0cebaaab8c636fbada504a87ebf26cb589
nitis <nitishvelu@gmail.com>
2021-09-17 22:21:13 +0530

    initial omuse project setup with create-next-app

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Omuse!</a>
         </h1>
 
         <p className={styles.description}>
@@ -23,7 +23,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>Documentation yeahh &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
