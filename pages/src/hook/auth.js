3faa642dd6a0cb27a2752135dbbd6b38d28a7ba2
nitis <nitishvelu@gmail.com>
2021-09-17 23:17:11 +0530

    global auth context via hooks

---
+++ pages/src/hook/auth.js
@@ -0,0 +1,25 @@
+import { createContext, useContext } from "react";
+
+const authContext = createContext();
+
+export default function useAuth(){
+    return useContext(authContext);
+}
+
+export function AuthProvider(props){
+    const[user,setUser]=useState(null);
+    const[error,setError]=useState("");
+     
+    const loginWithGoogle = async()=>{
+        const {error,user}=await AuthService.loginWithGoogle();
+        setUser(user ?? null);
+        setError(error ?? "");
+    };
+    const logout = async()=>{
+        await AuthService.logout();
+        setUser(null);
+    };
+    const value={user,error,loginWithGoogle,logout};
+
+    return <authContext.Provider value={value}{...props}/>;
+}
