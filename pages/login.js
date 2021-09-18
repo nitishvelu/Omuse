5d8e7b2b1dcfefb213ae63640c406dfa0470a319
nitis <nitishvelu@gmail.com>
2021-09-18 20:23:34 +0530

    routes

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import useAuth from '../src/hook/auth'
+
+export default function Login() {
+    const{user,loginWithGoogle,error} =useAuth();
+    return (
+        <div>
+            {error &&<h1>{error}</h1>}
+            <button onClick={loginWithGoogle}>google</button>
+            <h1>{user?.uid}</h1>
+            {console.log(user)}
+
+        </div>
+    )
+}
---
+++ src/layout/AuthStateChanged.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import useAuth from '../hook/auth';
+
+
+export default function AuthStateChanged() {
+    useEffect
+    return (
+        <div>
+            
+        </div>
+    )
+}
