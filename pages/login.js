1ed0ef48d5d094c7ea3e41ab66972919f6cad83b
nitis <nitishvelu@gmail.com>
2021-09-20 20:19:24 +0530

    authentication,protected/public routes,user data collection implemented

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import useAuth from '../src/hook/auth'
 import { withPublic } from '../src/hook/route';
 
-function Login() {
-    const{user,loginWithGoogle,error} =useAuth();
+function Login({auth}) {
+    const{user,loginWithGoogle,error} =auth;
     return (
         <div>
             {error &&<h1>{error}</h1>}
