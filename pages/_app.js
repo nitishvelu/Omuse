4eadda96b522b3ed9c2f993ab5ca7f10dcf3cb88
nitis <nitishvelu@gmail.com>
2021-10-25 15:21:29 +0530

    chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
 import '../src/config/firebase.config'
+import { ChakraProvider } from '@chakra-ui/react';
 import { AuthProvider } from '../src/hook/auth';
 import AuthStateChanged from '../src/layout/AuthStateChanged';
 
 function MyApp({ Component, pageProps }) {
   return (
+  <ChakraProvider>
   <AuthProvider>
     <AuthStateChanged>
     <Component {...pageProps} />
     </AuthStateChanged>
   </AuthProvider>
+  </ChakraProvider>
   );
 }
 
