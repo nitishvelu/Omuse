c2209eff3643bbe9582bf1d922d91fe43c2d3ab8
nitis <nitishvelu@gmail.com>
2021-10-25 17:45:10 +0530

    random sidebar

--- pages/app.js
+++ pages/app.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import { withProtected } from '../src/hook/route'
+import { Flex, Text, IconButton } from '@chakra-ui/react'
+import { FiMenu } from 'react-icons/fi'
+import Sidebar from '../components/Sidebar'
+
 
 function App({auth}) {
     const{user,logout}=auth;
     const photo=user?.photoURL;
     return (
-        <div>
-            <h1>make navbar here</h1>
-            <br></br>
-            <h1>{user?.email}</h1>
-            <br></br>
-            <h1>{user?.uid}</h1>
-            <br></br>
-            <img src={photo}/>
-            <br></br>
-            <h1>{user?.displayName}</h1>
-            <br></br>
-            <button onClick={logout}>Logout</button>
+        // <div>
+        //     <h1>make navbar here</h1>
+        //     <br></br>
+        //     <h1>{user?.email}</h1>
+        //     <br></br>
+        //     <h1>{user?.uid}</h1>
+        //     <br></br>
+        //     <img src={photo}/>
+        //     <br></br>
+        //     <h1>{user?.displayName}</h1>
+        //     <br></br>
+        //     <button onClick={logout}>Logout</button>
            
-        </div>
+        // </div>
+        <Flex w="100%">
+        <Sidebar />
+        <Flex
+          pos="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+        >
+          <Text>Click the
+            <IconButton
+              background="none"
+              _hover={{ background: 'none' }}
+              icon={<FiMenu />}
+            />
+          to resize the vertical navigation bar.</Text>
+        </Flex>
+      </Flex>
     )
 }
 export default withProtected(App);
