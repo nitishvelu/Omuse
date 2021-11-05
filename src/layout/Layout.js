b02f856e370ceecabfc3957b86da3eaf639b0027
nitis <nitishvelu@gmail.com>
2021-11-05 14:24:50 +0530

    sidebar not visible on login page

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from "../../components/Sidebar";
-import CheckUser from "../../components/cloudFirestore/createUser";
 import { Flex, Text, IconButton } from "@chakra-ui/react";
+import { useRouter } from 'next/router';
 
 
 export default function Layout({children}) {
+    const router=useRouter();
+    if(router.pathname!="/"){
     return (
     <>
     <Flex w="100%">
@@ -18,5 +20,7 @@ export default function Layout({children}) {
       </Flex>
     </Flex>
         </>
-    )
+    )}else{
+        return children;
+    }
 }
