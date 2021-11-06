5b4eaf5d6d631dc168840f8b1d1f8ebba7aa21c2
nitis <nitishvelu@gmail.com>
2021-11-06 19:34:29 +0530

    sidebar complete

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -39,8 +39,8 @@ export default function Sidebar() {
             left="5"
             h="95vh"
             marginTop="2.5vh"
-            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-            borderRadius={navSize == "small" ? "15px" : "30px"}
+            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.439)"
+            borderRadius={navSize == "small" ? "15px" : "19px"}
             w={navSize == "small" ? "75px" : "200px"}
             flexDir="column"
             justifyContent="space-between"
