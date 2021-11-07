6dc421fe0093f2fcf1a469ecc0da55935c0fd6bd
nitis <nitishvelu@gmail.com>
2021-11-07 16:18:34 +0530

    spinner

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,44 @@ import { AuthProvider } from "../src/hook/auth";
 import AuthStateChanged from "../src/layout/AuthStateChanged";
 import firebaseInit from "../src/config/firebase.config";
 import Layout from "../src/layout/Layout";
+import Router from "next/router";
+import React, { useEffect, useState } from "react";
+import { PushSpinner } from "react-spinners-kit";
+
+
 
 firebaseInit();
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] =useState(false);
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
   return (
     <ChakraProvider>
       <AuthProvider>
         <Layout>
         <AuthStateChanged>
+        {loading ? (
+          <PushSpinner size={40} color="#82AAAD" loading={loading} />
+        ) : (
           <Component {...pageProps} />
+        )}
         </AuthStateChanged>
         </Layout>
       </AuthProvider>
