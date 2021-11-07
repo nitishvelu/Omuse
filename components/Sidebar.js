9d47535ea984aaa4bf8b5539df0c31df3f9abd55
nitis <nitishvelu@gmail.com>
2021-11-07 19:42:55 +0530

    show upload button only to artist

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -17,7 +17,8 @@ import {
     FiGlobe,
     FiDisc,
     FiSun,
-    FiMoon
+    FiMoon,
+    FiUploadCloud
 } from 'react-icons/fi'
 import NavItem from '../components/NavItem';
 import useAuth from '../src/hook/auth';
@@ -32,9 +33,82 @@ export default function Sidebar() {
     const {user}=auth;
     const {colorMode,toggleColorMode}=useColorMode();
 
-    // if (typeof window !== 'undefined') {
-    //     localStorage.setItem('name',user?.displayName);
-    // }
+    
+    //conditinal rendering to check if user is artist to show upload
+    if(cookie.get("typeOfUser")=="Artist"){
+        return(
+        <Flex
+            pos="sticky"
+            left="5"
+            h="95vh"
+            marginTop="2.5vh"
+            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.439)"
+            borderRadius={navSize == "small" ? "15px" : "19px"}
+            w={navSize == "small" ? "75px" : "200px"}
+            flexDir="column"
+            justifyContent="space-between"
+        >
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize == "small" ? "center" : "flex-start"}
+                as="nav"
+            >
+                <IconButton
+                    background="none"
+                    mt={5}
+                    _hover={{ background: 'none' }}
+                    icon={<FiMenu />}
+                    onClick={() => {
+                        if (navSize == "small")
+                            changeNavSize("large")
+                        else
+                            changeNavSize("small")
+                    }}
+                />
+                <NavItem navSize={navSize} icon={FiHome} title="Dashboard" to="/app"/>
+                <NavItem navSize={navSize} icon={FiMusic} title="Playlists" to="/playlists" />
+                <NavItem navSize={navSize} icon={FiTrendingUp} title="Trending" to="/trending"/>
+                <NavItem navSize={navSize} icon={FiHeart} title="Liked" to="/liked" />
+                <NavItem navSize={navSize} icon={FiDisc} title="Genre" to="/genre"/>
+                <NavItem navSize={navSize} icon={FiGlobe} title="Language" to="/language"/>
+                <NavItem navSize={navSize} icon={FiLogOut} title="Logout" to="/logout"/>
+                <NavItem navSize={navSize} icon={FiUploadCloud} title="Upload" to="/upload"/>
+                
+                
+                
+                
+            </Flex>
+
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize == "small" ? "center" : "flex-start"}
+                mb={4}
+            >
+                 <IconButton
+                    background="none"
+                    mt={5}
+                    _hover={{ background: 'none' }}
+                    icon={colorMode=="light"?<FiSun/>:<FiMoon/>}
+                    onClick={toggleColorMode}
+                />
+                <Divider display={navSize == "small" ? "none" : "flex"} />
+                <Flex mt={4} align="center">
+                    <Avatar size="sm" src={user?.photoURL} />
+                    <Flex flexDir="column" ml={4} display={navSize == "small" ? "none" : "flex"}>
+                        <Heading as="h3" size="sm">{user?.displayName}</Heading>
+                        <Text color="gray">{cookie.get("typeOfUser")||"User"}</Text>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Flex>
+    )
+
+    }else{
+
     return (
         <Flex
             pos="sticky"
@@ -73,7 +147,7 @@ export default function Sidebar() {
                 <NavItem navSize={navSize} icon={FiDisc} title="Genre" to="/genre"/>
                 <NavItem navSize={navSize} icon={FiGlobe} title="Language" to="/language"/>
                 <NavItem navSize={navSize} icon={FiLogOut} title="Logout" to="/logout"/>
-               
+                
                 
                 
             </Flex>
@@ -102,5 +176,5 @@ export default function Sidebar() {
                 </Flex>
             </Flex>
         </Flex>
-    )
+    )}
 }
