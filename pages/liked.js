648e049d92776e754985f7cd1dbb0e9f87ca76df
nitis <nitishvelu@gmail.com>
2021-11-09 23:29:10 +0530

    got in h5-audio player source issues has to be fixed

--- pages/liked.js
+++ pages/liked.js
@@ -1,29 +1,33 @@
-import React from "react";
+// import React from "react";
 import { withProtected } from "../src/hook/route";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { FiMenu } from "react-icons/fi";
 import Sidebar from "../components/Sidebar";
 import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
-import { Box, Button, Heading, Text, VStack } from "@chakra-ui/layout";
+import { Box, Button, Text, VStack } from "@chakra-ui/layout";
+import Song from "../components/Songs";
+import { Heading } from "@chakra-ui/react";
 
-function Liked({ songids }) {
+function Liked({ songs_list }) {
+	// console.log(songs_list);
 	return (
 		<>
 			<Heading>Favourites</Heading>
 			<VStack>
-				{songids.map((txt) => (
-					<Box key={txt}>{txt}</Box>
-				))}
+				{Object.keys(songs_list).map((idx) => {
+					return <Song song_obj={songs_list[idx]} key={idx} />;
+				})}
 			</VStack>
 		</>
 	);
 }
 export default withProtected(Liked);
 
-export async function getServerSideProps(context) {
-	const { req } = context;
+export async function getServerSideProps({ req }) {
 	let songids = [];
+	let songs_list = [];
+
 	const ref = await firebase
 		.firestore()
 		.collection("user")
@@ -31,15 +35,41 @@ export async function getServerSideProps(context) {
 		.get()
 		.then((doc) => {
 			if (doc.exists) {
-				// songs.push(doc.data().email);
 				doc.data().liked_songs.forEach((element) => {
-					// console.log(1111111);
 					songids.push(element);
 				});
-				// console.log(songs)
 			} else {
 			}
 		});
-	console.log(songids);
-	return { props: { songids } };
+
+	for (let i = 0; i < songids.length; i++) {
+		const song_obj = new Object();
+		song_obj.id = songids[i];
+		const db = await firebase
+			.firestore()
+			.collection("song")
+			.doc(songids[i])
+			.get()
+			.then((doc) => {
+				if (doc.exists) {
+					song_obj.name = doc.data().name;
+					song_obj.genre = doc.data().genre;
+					song_obj.duration = doc.data().duration;
+					song_obj.year = doc.data().year;
+					song_obj.no_of_likes = doc.data().no_of_likes;
+					song_obj.no_of_streams = doc.data().no_of_streams;
+					song_obj.language = doc.data().language;
+					song_obj.ref = doc.data().cloud_reference;
+					song_obj.img = doc.data().art;
+					// song_obj.album = doc.data().album;
+					songs_list.push(song_obj);
+				}
+			});
+	}
+	// console.log(songs_list);
+	// const songs = [songs_list];
+	// console.log(typeof songs);
+	// songs_list = Array(songs_list);
+	// console.log(typeof songs_list);
+	return { props: { songs_list } };
 }
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,6 +2,8 @@ import React from "react";
 import Sidebar from "../../components/Sidebar";
 import { HStack, VStack, Text, IconButton } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 export default function Layout({ children }) {
 	const router = useRouter();
@@ -16,7 +18,7 @@ export default function Layout({ children }) {
 				>
 					<Sidebar />
 					<VStack
-						px={0}
+						pr={7}
 						pt={9}
 						width='full'
 						height='full'
@@ -25,6 +27,12 @@ export default function Layout({ children }) {
 						alignItems='flex-start'
 					>
 						{children}
+						<AudioPlayer
+							class='audioPlayer'
+							src='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/after%20hours%2F09%20Blinding%20Lights.mp3?alt=media&token=77ad2d5f-6f00-40bb-9e75-cb8401ee139d'
+							onPlay={(e) => console.log("onPlay")}
+							// other props here
+						/>
 					</VStack>
 				</HStack>
 			</>
