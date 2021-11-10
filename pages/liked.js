dcff97870e00b37329408aecc0419f2d6a5f56d7
Srinivas-VJ <srinivasvj01@gmail.com>
2021-11-10 17:21:34 +0530

    fetching artist and album from db for song object

--- pages/liked.js
+++ pages/liked.js
@@ -10,66 +10,79 @@ import Song from "../components/Songs";
 import { Heading } from "@chakra-ui/react";
 
 function Liked({ songs_list }) {
-	// console.log(songs_list);
-	return (
-		<>
-			<Heading>Favourites</Heading>
-			<VStack>
-				{Object.keys(songs_list).map((idx) => {
-					return <Song song_obj={songs_list[idx]} key={idx} />;
-				})}
-			</VStack>
-		</>
-	);
+  console.log(songs_list);
+  return (
+    <>
+      <Heading>Favourites</Heading>
+      <VStack>
+        {Object.keys(songs_list).map((idx) => {
+          return <Song song_obj={songs_list[idx]} key={idx} />;
+        })}
+      </VStack>
+    </>
+  );
 }
 export default withProtected(Liked);
 
 export async function getServerSideProps({ req }) {
-	let songids = [];
-	let songs_list = [];
+  let songids = [];
+  let songs_list = [];
 
-	const ref = await firebase
-		.firestore()
-		.collection("user")
-		.doc(req.cookies.uid)
-		.get()
-		.then((doc) => {
-			if (doc.exists) {
-				doc.data().liked_songs.forEach((element) => {
-					songids.push(element);
-				});
-			} else {
-			}
-		});
+  const ref = await firebase
+    .firestore()
+    .collection("user")
+    .doc(req.cookies.uid)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        doc.data().liked_songs.forEach((element) => {
+          songids.push(element);
+        });
+      } else {
+      }
+    });
 
-	for (let i = 0; i < songids.length; i++) {
-		const song_obj = new Object();
-		song_obj.id = songids[i];
-		const db = await firebase
-			.firestore()
-			.collection("song")
-			.doc(songids[i])
-			.get()
-			.then((doc) => {
-				if (doc.exists) {
-					song_obj.name = doc.data().name;
-					song_obj.genre = doc.data().genre;
-					song_obj.duration = doc.data().duration;
-					song_obj.year = doc.data().year;
-					song_obj.no_of_likes = doc.data().no_of_likes;
-					song_obj.no_of_streams = doc.data().no_of_streams;
-					song_obj.language = doc.data().language;
-					song_obj.ref = doc.data().cloud_reference;
-					song_obj.img = doc.data().art;
-					// song_obj.album = doc.data().album;
-					songs_list.push(song_obj);
-				}
-			});
-	}
-	// console.log(songs_list);
-	// const songs = [songs_list];
-	// console.log(typeof songs);
-	// songs_list = Array(songs_list);
-	// console.log(typeof songs_list);
-	return { props: { songs_list } };
+  for (let i = 0; i < songids.length; i++) {
+    const song_obj = new Object();
+    song_obj.id = songids[i];
+    const db = await firebase
+      .firestore()
+      .collection("song")
+      .doc(songids[i])
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          song_obj.name = doc.data().name;
+          song_obj.genre = doc.data().genre;
+          song_obj.duration = doc.data().duration;
+          song_obj.year = doc.data().year;
+          song_obj.no_of_likes = doc.data().no_of_likes;
+          song_obj.no_of_streams = doc.data().no_of_streams;
+          song_obj.language = doc.data().language;
+          song_obj.ref = doc.data().cloud_reference;
+          song_obj.img = doc.data().art;
+          //   song_obj.album = firebase
+          let album = doc.data().album;
+
+          album.get().then((al) => {
+            if (al.exists) {
+              song_obj.album = al.data().name;
+              let artist = al.data().artist;
+              artist.get().then((art) => {
+                if (art.exists) {
+                  song_obj.artist = art.data().name;
+                }
+              });
+            }
+          });
+          songs_list.push(song_obj);
+        }
+      });
+  }
+  // console.log(songs_list);
+  // const songs = [songs_list];
+  // console.log(typeof songs);
+  // songs_list = Array(songs_list);
+  // console.log(typeof songs_list);
+  return { props: { songs_list } };
 }
