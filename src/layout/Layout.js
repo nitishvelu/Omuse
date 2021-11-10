d6e06ebad55a92ba2ddae5971fd0de03a155ad1b
nitis <nitishvelu@gmail.com>
2021-11-10 15:20:03 +0530

    fixed layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,6 +4,17 @@ import { HStack, VStack, Grid, GridItem, Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import {
+	FiChevronLeft,
+	FiChevronRight,
+	FiFastForward,
+	FiPause,
+	FiPauseCircle,
+	FiPlay,
+	FiPlayCircle,
+	FiRepeat,
+} from "react-icons/fi";
+import { AiOutlineDoubleRight, AiOutlineDoubleLeft } from "react-icons/ai";
 
 export default function Layout({ children }) {
 	const router = useRouter();
@@ -28,18 +39,22 @@ export default function Layout({ children }) {
 					>
 						{children}
 						<Box
+							as={AudioPlayer}
 							width='90%'
 							height='auto'
 							position='fixed'
 							bottom={4}
-						>
-							<AudioPlayer
-								class='audioPlayer'
-								src='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/after%20hours%2F09%20Blinding%20Lights.mp3?alt=media&token=77ad2d5f-6f00-40bb-9e75-cb8401ee139d'
-								onPlay={(e) => console.log("onPlay")}
-								// other props here
-							/>
-						</Box>
+							class='audioPlayer'
+							src='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/after%20hours%2F09%20Blinding%20Lights.mp3?alt=media&token=77ad2d5f-6f00-40bb-9e75-cb8401ee139d'
+							onPlay={(e) => console.log("onPlay")}
+							b='grey'
+							customIcons={{
+								play: <FiPlayCircle />,
+								pause: <FiPauseCircle />,
+								rewind: <AiOutlineDoubleLeft />,
+								forward: <AiOutlineDoubleRight />,
+							}}
+						/>
 					</VStack>
 				</HStack>
 			</>
