65246fb2e9b4405f1b5540050448002410e24620
nitis <nitishvelu@gmail.com>
2021-11-11 20:47:14 +0530

    lol srini error

--- pages/liked.js
+++ pages/liked.js
@@ -10,107 +10,107 @@ import Song from "../components/Songs";
 import { Heading } from "@chakra-ui/react";
 
 function Liked({ songs_list }) {
-  // console.log(songs_list);
-  return (
-    <>
-      <Heading>Favourites</Heading>
+	// console.log(songs_list);
+	return (
+		<>
+			<Heading>Favourites</Heading>
 
-      <SimpleGrid
-        minChildWidth="300px"
-        overflowX="auto"
-        rounded="1g"
-        height="80%"
-        width="full"
-        spacingX={0}
-        spacingY={9}
-        css={{
-          "&::-webkit-scrollbar": {
-            width: "7px",
-          },
-          "&::-webkit-scrollbar-track": {
-            width: "7px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            background: "pink",
-            borderRadius: "15px",
-          },
-        }}
-      >
-        {Object.keys(songs_list).map((idx) => {
-          return <Song song_obj={songs_list[idx]} key={idx} />;
-        })}
-      </SimpleGrid>
-    </>
-  );
+			<SimpleGrid
+				minChildWidth='300px'
+				overflowX='auto'
+				rounded='1g'
+				height='80%'
+				width='full'
+				spacingX={0}
+				spacingY={9}
+				css={{
+					"&::-webkit-scrollbar": {
+						width: "7px",
+					},
+					"&::-webkit-scrollbar-track": {
+						width: "7px",
+					},
+					"&::-webkit-scrollbar-thumb": {
+						background: "pink",
+						borderRadius: "15px",
+					},
+				}}
+			>
+				{Object.keys(songs_list).map((idx) => {
+					return <Song song_obj={songs_list[idx]} key={idx} />;
+				})}
+			</SimpleGrid>
+		</>
+	);
 }
 export default withProtected(Liked);
 
 export async function getServerSideProps({ req }) {
-  let songids = [];
-  let songs_list = [];
+	let songids = [];
+	let songs_list = [];
 
-  const ref = await firebase
-    .firestore()
-    .collection("user")
-    .doc(req.cookies.uid)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        doc.data().liked_songs.forEach((element) => {
-          songids.push(element);
-        });
-      } else {
-      }
-    });
+	const ref = await firebase
+		.firestore()
+		.collection("user")
+		.doc(req.cookies.uid)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				doc.data().liked_songs?.forEach((element) => {
+					songids.push(element);
+				});
+			} else {
+			}
+		});
 
-  let artists = [];
-  let albums = [];
+	let artists = [];
+	let albums = [];
 
-  //fetching song data
-  for (let i = 0; i < songids.length; i++) {
-    const song_obj = new Object();
-    song_obj.id = songids[i];
-    const db = await firebase
-      .firestore()
-      .collection("song")
-      .doc(songids[i])
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          song_obj.name = doc.data().name;
-          song_obj.genre = doc.data().genre;
-          song_obj.duration = doc.data().duration;
-          song_obj.year = doc.data().year;
-          song_obj.no_of_likes = doc.data().no_of_likes;
-          song_obj.no_of_streams = doc.data().no_of_streams;
-          song_obj.language = doc.data().language;
-          song_obj.ref = doc.data().cloud_reference;
-          song_obj.img = doc.data().art;
-          albums.push(doc.data().album);
-          //   song_obj.album = firebase
-          songs_list.push(song_obj);
-        }
-      });
-  }
+	//fetching song data
+	for (let i = 0; i < songids.length; i++) {
+		const song_obj = new Object();
+		song_obj.id = songids[i];
+		const db = await firebase
+			.firestore()
+			.collection("song")
+			.doc(songids[i])
+			.get()
+			.then((doc) => {
+				if (doc.exists) {
+					song_obj.name = doc.data().name;
+					song_obj.genre = doc.data().genre;
+					song_obj.duration = doc.data().duration;
+					song_obj.year = doc.data().year;
+					song_obj.no_of_likes = doc.data().no_of_likes;
+					song_obj.no_of_streams = doc.data().no_of_streams;
+					song_obj.language = doc.data().language;
+					song_obj.ref = doc.data().cloud_reference;
+					song_obj.img = doc.data().art;
+					albums.push(doc.data().album);
+					//   song_obj.album = firebase
+					songs_list.push(song_obj);
+				}
+			});
+	}
 
-  // getting albums
-  for (let i = 0; i < albums.length; i++) {
-    const db = await albums[i].get().then((doc) => {
-      if (doc.exists) {
-        songs_list[i].album = doc.data().name;
-        artists.push(doc.data().artist);
-      }
-    });
-  }
+	// getting albums
+	for (let i = 0; i < albums.length; i++) {
+		const db = await albums[i].get().then((doc) => {
+			if (doc.exists) {
+				songs_list[i].album = doc.data().name;
+				artists.push(doc.data().artist);
+			}
+		});
+	}
 
-  // getting artists
-  for (let i = 0; i < albums.length; i++) {
-    const db = await artists[i].get().then((doc) => {
-      if (doc.exists) {
-        songs_list[i].artist_name = doc.data().name;
-        songs_list[i].artist_id = doc.id;
-      }
-    });
-  }
-  return { props: { songs_list } };
+	// getting artists
+	for (let i = 0; i < albums.length; i++) {
+		const db = await artists[i].get().then((doc) => {
+			if (doc.exists) {
+				songs_list[i].artist_name = doc.data().name;
+				songs_list[i].artist_id = doc.id;
+			}
+		});
+	}
+	return { props: { songs_list } };
 }
