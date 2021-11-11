9ccb1ee96ab93aaadd71dc734b556f1177d27275
nitis <nitishvelu@gmail.com>
2021-11-11 20:28:37 +0530

    like unlike song

--- components/Songs.js
+++ components/Songs.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { FiPlay, FiPause, FiHeart } from "react-icons/fi";
 import {
-  Flex,
-  Link,
-  Grid,
-  GridItem,
-  Text,
-  IconButton,
-  Icon,
-  Image,
+	Flex,
+	Link,
+	Grid,
+	GridItem,
+	Text,
+	IconButton,
+	Icon,
+	Image,
 } from "@chakra-ui/react";
 import firebase from "firebase/app";
 import NextLink from "next/link";
@@ -19,71 +19,74 @@ var is_First_play = true;
 import { motion } from "framer-motion";
 
 const incrementStreams = (song_id) => {
-  const doc_ref = firebase.firestore().collection("song").doc(song_id);
-  doc_ref.update({ no_of_streams: FieldValue.increment(1) });
+	const doc_ref = firebase.firestore().collection("song").doc(song_id);
+	doc_ref.update({ no_of_streams: FieldValue.increment(1) });
 };
 
 export default function Song({ song_obj }) {
-  console.group(song_obj);
-  return (
-    <Grid
-      h="12vh"
-      w="300px"
-      templateRows="repeat(5, 1fr)"
-      templateColumns="repeat(5, 1fr)"
-      gap={1}
-      rounded="15px"
-    >
-      <GridItem rowSpan={5} colSpan={2}>
-        <Link>
-          <Image
-            src={song_obj.img}
-            alt="album image"
-            objectFit="cover"
-            boxSize="12vh"
-            borderRadius="30"
-            onClick={function clickHandler(e) {
-              const div = document.getElementById("musicPlayer");
-              const player = div.childNodes[0];
-              player.childNodes[0].src = song_obj.ref;
-              player.childNodes[0].play();
-              incrementStreams(song_obj.id);
-            }}
-            fallbackSrc="https://wallpaperaccess.com/full/2374217.png"
-          />
-        </Link>
-      </GridItem>
-      <GridItem rowSpan={2} colSpan={3}>
-        <NextLink href={"/song/" + song_obj.id} passHref>
-          <Link _hover={{ textDecor: "none" }} _focus={{ boxShadow: "none" }}>
-            <Text
-              fontSize="3xl"
-              isTruncated
-              _hover={{
-                color: "teal.500",
-                fontWeight: "semibold",
-              }}
-            >
-              {song_obj.name}
-            </Text>
-          </Link>
-        </NextLink>
-      </GridItem>
+	console.group(song_obj);
+	return (
+		<Grid
+			h='12vh'
+			w='300px'
+			templateRows='repeat(5, 1fr)'
+			templateColumns='repeat(5, 1fr)'
+			gap={1}
+			rounded='15px'
+		>
+			<GridItem rowSpan={5} colSpan={2}>
+				<Link>
+					<Image
+						src={song_obj.img}
+						alt='album image'
+						objectFit='cover'
+						boxSize='12vh'
+						borderRadius='30'
+						onClick={function clickHandler(e) {
+							const div = document.getElementById("musicPlayer");
+							const player = div.childNodes[0];
+							player.childNodes[0].src = song_obj.ref;
+							player.childNodes[0].play();
+							incrementStreams(song_obj.id);
+						}}
+						fallbackSrc='https://wallpaperaccess.com/full/2374217.png'
+					/>
+				</Link>
+			</GridItem>
+			<GridItem rowSpan={2} colSpan={3}>
+				<NextLink href={"/song/" + song_obj.id} passHref>
+					<Link
+						_hover={{ textDecor: "none" }}
+						_focus={{ boxShadow: "none" }}
+					>
+						<Text
+							fontSize='3xl'
+							isTruncated
+							_hover={{
+								color: "teal.500",
+								fontWeight: "semibold",
+							}}
+						>
+							{song_obj.name}
+						</Text>
+					</Link>
+				</NextLink>
+			</GridItem>
 
-      <GridItem rowSpan={1} colSpan={3} rowEnd={5} colStart={3}>
-        <Text as="sup" color="gray.500">
-          {song_obj.duration}
-        </Text>
-      </GridItem>
-      <GridItem rowSpan={1} colSpan={3}>
-        <NextLink href={"/artist/" + song_obj.artist_id}>
-          <Link>
-            <Text as="sub" color="gray.500">
-              {song_obj.artist_name}
-            </Text>
-          </Link>
-        </NextLink>
-      </GridItem>
-    </Grid>
-  );
+			<GridItem rowSpan={1} colSpan={3} rowEnd={5} colStart={3}>
+				<Text as='sup' color='gray.500'>
+					{song_obj.duration}
+				</Text>
+			</GridItem>
+			<GridItem rowSpan={1} colSpan={3}>
+				<NextLink href={"/artist/" + song_obj.artist_id}>
+					<Link>
+						<Text as='sub' color='gray.500'>
+							{song_obj.artist_name}
+						</Text>
+					</Link>
+				</NextLink>
+			</GridItem>
+		</Grid>
+	);
 }
--- pages/song/[songid].js
+++ pages/song/[songid].js
@@ -7,65 +7,102 @@ import { Box, Button, Text, VStack, IconButton } from "@chakra-ui/layout";
 import { HStack } from "@chakra-ui/react";
 import { CloseButton } from "@chakra-ui/react";
 import cookie from "js-cookie";
+import { useState } from "react";
 // import { FiHeart } from "react-icons/fi";
+function Songdetails({ song, liked }) {
+	const db = firebase.firestore();
+	const [noLikes, setNoLikes] = useState(song[5]);
+	const [isLiked, setIsLiked] = useState(liked);
 
-function Songdetails({ song }) {
-  return (
-    <>
-      <HStack>
-        <VStack>
-          {song.map((txt, i) => (
-            <>
-              <Text fontWeight="bold" fontSize="xl" key={i}>
-                {txt}
-              </Text>
-            </>
-          ))}
-          <button
-            onClick={function (e) {
-              const db = firebase.firestore();
-              db.collection("user")
-                .doc(cookie.get("uid"))
-                .update({
-                  liked_songs: firebase.firestore.FieldValue.arrayUnion(
-                    song[0]
-                  ),
-                });
-            }}
-          >
-            like
-          </button>
-        </VStack>
-        <CloseButton size="lg" />
-      </HStack>
-    </>
-  );
+	const [likeUnlike, setlikeUnlike] = useState(liked ? "unlike" : "like");
+
+	const handleClick = () => {
+		//unliking song
+		if (isLiked) {
+			setIsLiked(false);
+			setNoLikes(noLikes - 1);
+			setlikeUnlike("like");
+			db.collection("user")
+				.doc(cookie.get("uid"))
+				.update({
+					liked_songs: firebase.firestore.FieldValue.arrayRemove(
+						song[0]
+					),
+				});
+		}
+		//liking song
+		else {
+			setIsLiked(true);
+			setNoLikes(noLikes + 1);
+			setlikeUnlike("unlike");
+			db.collection("user")
+				.doc(cookie.get("uid"))
+				.update({
+					liked_songs: firebase.firestore.FieldValue.arrayUnion(
+						song[0]
+					),
+				});
+		}
+	};
+	return (
+		<>
+			<HStack>
+				<VStack>
+					{song.map((txt, i) => (
+						<>
+							<Text fontWeight='bold' fontSize='xl' key={i}>
+								{txt}
+							</Text>
+						</>
+					))}
+					<Text>{noLikes}</Text>
+					<button onClick={handleClick}>{likeUnlike}</button>
+				</VStack>
+				<CloseButton size='lg' />
+			</HStack>
+		</>
+	);
 }
 export default withProtected(Songdetails);
 
 export async function getServerSideProps(context) {
-  const { params, req } = context;
-  let song = [];
-  let doesnotexists = false;
-  await firebase
-    .firestore()
-    .collection("song")
-    .doc(params.songid)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        song.push(doc.id);
-        song.push(doc.data().name);
-        song.push(doc.data().year);
-        song.push(doc.data().language);
-        song.push(doc.data().genre);
-      } else {
-        doesnotexists = true;
-      }
-    });
-  if (doesnotexists) {
-    return { notFound: true };
-  }
+	const { params, req } = context;
+	let song = [];
+	let doesnotexists = false;
+	await firebase
+		.firestore()
+		.collection("song")
+		.doc(params.songid)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				song.push(doc.id);
+				song.push(doc.data().name);
+				song.push(doc.data().year);
+				song.push(doc.data().language);
+				song.push(doc.data().genre);
+				song.push(doc.data().no_of_likes);
+			} else {
+				doesnotexists = true;
+			}
+		});
 
-  return { props: { song } };
+	//checking if song is liked by the requested user
+	let liked = false;
+	await firebase
+		.firestore()
+		.collection("user")
+		.doc(req.cookies.uid)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				liked = doc.data().liked_songs?.includes(params.songid);
+			} else {
+			}
+		});
+	if (doesnotexists) {
+		return { notFound: true };
+	}
+
+	return { props: { song, liked } };
 }
