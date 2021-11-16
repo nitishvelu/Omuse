f13f7e84393bb2f88800cd14e74c4530b108392c
nitis <nitishvelu@gmail.com>
2021-11-16 21:57:02 +0530

    trying out meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,43 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { withPublic } from '../src/hook/route';
+import { withPublic } from "../src/hook/route";
 import { Button } from "@chakra-ui/button";
 
+function Home({ auth }) {
+	const { loginWithGoogle, error } = auth;
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Omuse</title>
+				<meta
+					name='description'
+					content='A multi user music platform'
+				/>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					Welcome to{" "}
+					<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+						Omuse!
+					</a>
+				</h1>
 
-function Home({auth}) {
-  const{loginWithGoogle,error} =auth;
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Omuse</title>
-        <meta name="description" content="A multi user music platform"/>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+				<div>
+					{error && <h1>{error}</h1>}
+					<Button onClick={loginWithGoogle}>Login</Button>
+				</div>
+			</main>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to{" "}
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Omuse!</a>
-        </h1>
-        
-        <div>
-            {error &&<h1>{error}</h1>}
-            <Button onClick={loginWithGoogle}>Login</Button>
-        </div>
-
-       
-      </main>
-
-      {/* <footer className={styles.footer}>
+			{/* <footer className={styles.footer}>
         <h1></h1>
       </footer> */}
-    </div>
-  );
+		</div>
+	);
 }
 export default withPublic(Home);
