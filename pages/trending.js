0f0a1df2275f79ba9a04e854c24add72fd72ebdf
Srinivas-VJ <srinivasvj01@gmail.com>
2021-11-19 14:38:44 +0530

    trending pagge

--- pages/trending.js
+++ pages/trending.js
@@ -48,36 +48,79 @@ export default withProtected(Trending);
 export async function getStaticProps({ req }) {
   let songids = [];
   let songs_list = [];
-  let artists = [];
-  let albums = [];
-  const db = firebase.firestore();
 
-  const songs = await db
+  //   const db = firebase.firestore();
+  const ref = await firebase
+    .firestore()
     .collection("song")
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
         const song_obj = new Object();
         song_obj.id = doc.id;
-        // console.log(doc.id, " => ", doc.data());
         song_obj.name = doc.data().name;
         song_obj.genre = doc.data().genre;
+        // song_obj.duration = doc.data().duration;
         song_obj.year = doc.data().year;
         song_obj.no_of_likes = doc.data().no_of_likes;
         song_obj.no_of_streams = doc.data().no_of_streams;
         song_obj.language = doc.data().language;
         song_obj.ref = doc.data().cloud_reference;
         song_obj.img = doc.data().art;
-        // albums.push(doc.data().album);
+        albums.push(doc.data().album);
         //   song_obj.album = firebase
+        songs_list.push(song_obj);
       });
     });
+  console.log(songs_list);
+  //   const ref = await firebase
+  //     .firestore()
+  //     .collection("song")
+  //     .doc()
+  //     .get()
+  //     .then((doc) => {
+  //       if (doc.exists) {
 
-  //   console.log(songs_list);
+  //           songids.push(element);
 
-  // getting albums
-  //   for (let i = 0; i < songs_list.length; i++) {
+  //       } else {
+  //       }
+  //     });
+
+  //   let artists = [];
+  //   let albums = [];
+
+  //fetching song data
+  //   for (let i = 0; i < songids.length; i++) {
+  //     const song_obj = new Object();
+  //     song_obj.id = songids[i];
+  //     const db = await firebase
+  //       .firestore()
+  //       .collection("song")
+  //       .doc(songids[i])
+  //       .get()
+  //       .then((doc) => {
+  //         if (doc.exists) {
+  //           song_obj.name = doc.data().name;
+  //           song_obj.genre = doc.data().genre;
+  //           // song_obj.duration = doc.data().duration;
+  //           song_obj.year = doc.data().year;
+  //           song_obj.no_of_likes = doc.data().no_of_likes;
+  //           song_obj.no_of_streams = doc.data().no_of_streams;
+  //           song_obj.language = doc.data().language;
+  //           song_obj.ref = doc.data().cloud_reference;
+  //           song_obj.img = doc.data().art;
+  //           albums.push(doc.data().album);
+  //           //   song_obj.album = firebase
+  //           songs_list.push(song_obj);
+  //         }
+  //       });
+  //   }
+
+  //   // getting albums
+  //   for (let i = 0; i < albums.length; i++) {
   //     const db = await albums[i].get().then((doc) => {
   //       if (doc.exists) {
   //         songs_list[i].album = doc.data().name;
