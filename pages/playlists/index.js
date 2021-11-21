8de8ebb0499ce90f94452e92eac2e3f5fd73c6ea
nitis <nitishvelu@gmail.com>
2021-11-21 23:04:27 +0530

    responsive create playlist

--- pages/playlists/index.js
+++ pages/playlists/index.js
@@ -24,100 +24,102 @@ import Song from "../../components/Songs";
 import NextLink from "next/link";
 import cookie from "js-cookie";
 
-function BasicUsage() {
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	const [value, setValue] = React.useState("");
-	const handleChange = (event) => setValue(event.target.value);
+function Playlists({ playlist_details }) {
+	//handling newly created playlist using use state
+	const [newPlaylist, setPlaylist] = React.useState("");
+	function BasicUsage() {
+		const { isOpen, onOpen, onClose } = useDisclosure();
+		const [value, setValue] = React.useState("");
+		const handleChange = (event) => setValue(event.target.value);
 
-	return (
-		<>
-			<Button
-				onClick={onOpen}
-				leftIcon={<FiPlusCircle />}
-				variant='outline'
-			>
-				Create New Playlist
-			</Button>
+		return (
+			<>
+				<Button
+					onClick={onOpen}
+					leftIcon={<FiPlusCircle />}
+					variant='outline'
+				>
+					Create New Playlist
+				</Button>
 
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Enter Playlist Name</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						<Input
-							value={value}
-							onChange={handleChange}
-							size='sm'
-						/>
-					</ModalBody>
+				<Modal isOpen={isOpen} onClose={onClose}>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader>Enter Playlist Name</ModalHeader>
+						<ModalCloseButton />
+						<ModalBody>
+							<Input
+								value={value}
+								onChange={handleChange}
+								size='sm'
+							/>
+						</ModalBody>
 
-					<ModalFooter>
-						<Button mr={3} onClick={onClose}>
-							Cancel
-						</Button>
-						<Button
-							onClick={(e) => {
-								console.log(value);
-								var today = new Date();
-								var dd = String(today.getDate()).padStart(
-									2,
-									"0"
-								);
-								var mm = String(today.getMonth() + 1).padStart(
-									2,
-									"0"
-								); //January is 0!
-								var yyyy = today.getFullYear();
-								today = mm + "/" + dd + "/" + yyyy;
+						<ModalFooter>
+							<Button mr={3} onClick={onClose}>
+								Cancel
+							</Button>
+							<Button
+								onClick={(e) => {
+									console.log(value);
+									var today = new Date();
+									var dd = String(today.getDate()).padStart(
+										2,
+										"0"
+									);
+									var mm = String(
+										today.getMonth() + 1
+									).padStart(2, "0"); //January is 0!
+									var yyyy = today.getFullYear();
+									today = mm + "/" + dd + "/" + yyyy;
 
-								console.log(today);
-								const db = firebase.firestore();
+									console.log(today);
+									const db = firebase.firestore();
 
-								// create playlist
-								var match = document.cookie.match(
-									new RegExp("(^| )" + "uid" + "=([^;]+)")
-								);
-								if (match) {
-									console.log(match[2]);
-									let playlist = db
-										.collection("playlist")
-										.doc();
-									playlist.set({
-										name: value,
-										songs: [],
-										date: today,
-										creator: cookie.get("name"),
-										creator_id: cookie.get("uid"),
-									});
-									db.collection("user")
-										.doc(match[2])
-										.update({
-											playlists:
-												firebase.firestore.FieldValue.arrayUnion(
-													playlist
-												),
+									// create playlist
+									var match = document.cookie.match(
+										new RegExp("(^| )" + "uid" + "=([^;]+)")
+									);
+									if (match) {
+										console.log(match[2]);
+										let playlist = db
+											.collection("playlist")
+											.doc();
+										playlist.set({
+											name: value,
+											songs: [],
+											date: today,
+											creator: cookie.get("name"),
+											creator_id: cookie.get("uid"),
 										});
-								}
-								onClose();
+										db.collection("user")
+											.doc(match[2])
+											.update({
+												playlists:
+													firebase.firestore.FieldValue.arrayUnion(
+														playlist
+													),
+											});
+									}
+									onClose();
+									setPlaylist(value);
 
-								//reloading the page after 3 seconds to see changes something else can be done
-								setInterval(function () {
-									location.reload();
-								}, 3000);
-							}}
-						>
-							Create
-						</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-		</>
-	);
-}
-
-function Liked({ playlist_details }) {
+									//reloading the page after 3 seconds to see changes something else can be done
+									// setInterval(function () {
+									// 	location.reload();
+									// }, 3000);
+								}}
+							>
+								Create
+							</Button>
+						</ModalFooter>
+					</ModalContent>
+				</Modal>
+			</>
+		);
+	}
 	console.log(playlist_details);
+
 	return (
 		<>
 			<Heading>Playlists</Heading>
@@ -153,6 +155,23 @@ function Liked({ playlist_details }) {
 								</div>
 							);
 						})}
+						<div key={newPlaylist}>
+							<NextLink
+								href={"/playlists/" + newPlaylist}
+								passHref
+								key={newPlaylist}
+							>
+								<Link
+									_hover={{ textDecor: "none" }}
+									_focus={{ boxShadow: "none" }}
+									key={newPlaylist}
+								>
+									<Text fontSize='4xl' key={newPlaylist}>
+										{newPlaylist}
+									</Text>
+								</Link>
+							</NextLink>
+						</div>
 						{BasicUsage()}
 					</VStack>
 				</>
@@ -160,7 +179,7 @@ function Liked({ playlist_details }) {
 		</>
 	);
 }
-export default withProtected(Liked);
+export default withProtected(Playlists);
 
 export async function getServerSideProps({ req }) {
 	let playlists = [];
