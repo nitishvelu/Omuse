dfa1b5ca448d948fb0dd5f1d747437a9aa3f40fb
nitis <nitishvelu@gmail.com>
2021-11-22 23:06:17 +0530

    formatting

--- pages/logout.js
+++ pages/logout.js
@@ -2,25 +2,32 @@ import { Button } from "@chakra-ui/button";
 import React from "react";
 import { withProtected } from "../src/hook/route";
 import cookie from "js-cookie";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-
-const MotionButton = motion(Button)
+const MotionButton = motion(Button);
 
 function Logout({ auth }) {
-  const { logout } = auth;
-  return (
-    <div>
-      <MotionButton 
-      drag="x"
-      dragConstraints={{ left: -0, right: 0 }}
-      _focus={{ boxShadow: "none", }}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }} 
-      animate={{ rotate: 360 }}
-      transition={{ duration: 0.1 }}
-      onClick={()=>{logout();cookie.remove("uid");cookie.remove("name");cookie.remove("typeOfUser")}}>Logout</MotionButton>
-    </div>
-  );
+	const { logout } = auth;
+	return (
+		<div>
+			<MotionButton
+				drag='x'
+				dragConstraints={{ left: -0, right: 0 }}
+				_focus={{ boxShadow: "none" }}
+				whileHover={{ scale: 1.1 }}
+				whileTap={{ scale: 0.9 }}
+				animate={{ rotate: 360 }}
+				transition={{ duration: 0.1 }}
+				onClick={() => {
+					logout();
+					cookie.remove("uid");
+					cookie.remove("name");
+					cookie.remove("typeOfUser");
+				}}
+			>
+				Logout
+			</MotionButton>
+		</div>
+	);
 }
 export default withProtected(Logout);
