ae39139666a9a488ae62d1bff1e949a646c5cf3a
nitis <nitishvelu@gmail.com>
2021-11-22 13:42:51 +0530

    added the same layout for languages TODO make the gridbox component fancy

--- pages/language/index.js
+++ pages/language/index.js
@@ -1,130 +1,52 @@
 import React from "react";
-import { Link, Grid, GridItem, Text, Image, Heading } from "@chakra-ui/react";
+import {
+	Link,
+	SimpleGrid,
+	GridItem,
+	Text,
+	Image,
+	Heading,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 
 import { motion } from "framer-motion";
 
 const MText = motion(Text);
 const MImage = motion(Image);
+import GenreBox from "../../components/GenreBox";
 
 export default function Language() {
-  return (
-    <>
-      <Heading>Languages </Heading>
-      {/* change style for lang */}
-      <Grid
-        h={["12vh", "12vh", "16vh"]}
-        w={["300px", "300px", "400px"]}
-        // templateRows="repeat(5, 1fr)"
-        // templateColumns="repeat(5, 1fr)"
-        gap={20}
-        rounded="md"
-      >
-        {/* lang #1 */}
-        <GridItem>
-          <NextLink href={"/language/english"} passHref>
-            <Link _hover={{ textDecor: "none" }} _focus={{ boxShadow: "none" }}>
-              <MText
-                fontSize={["3xl", "3xl", "4xl"]}
-                isTruncated
-                margin={0}
-                padding={0}
-                whileHover={{
-                  scale: 1.009,
-                  color: "#AEC8CA",
-                }}
-                whileTap={{ scale: 0.96 }}
-              >
-                English
-              </MText>
-            </Link>
-          </NextLink>
-        </GridItem>
-
-        {/* lang #2 */}
-        <GridItem>
-          <NextLink href={"/language/kannada"} passHref>
-            <Link _hover={{ textDecor: "none" }} _focus={{ boxShadow: "none" }}>
-              <MText
-                fontSize={["3xl", "3xl", "4xl"]}
-                isTruncated
-                margin={0}
-                padding={0}
-                whileHover={{
-                  scale: 1.009,
-                  color: "#AEC8CA",
-                }}
-                whileTap={{ scale: 0.96 }}
-              >
-                Kannada
-              </MText>
-            </Link>
-          </NextLink>
-        </GridItem>
-
-        {/* lang #3 */}
-        <GridItem>
-          <NextLink href={"/language/tamil"} passHref>
-            <Link _hover={{ textDecor: "none" }} _focus={{ boxShadow: "none" }}>
-              <MText
-                fontSize={["3xl", "3xl", "4xl"]}
-                isTruncated
-                margin={0}
-                padding={0}
-                whileHover={{
-                  scale: 1.009,
-                  color: "#AEC8CA",
-                }}
-                whileTap={{ scale: 0.96 }}
-              >
-                Tamil
-              </MText>
-            </Link>
-          </NextLink>
-        </GridItem>
-
-        {/* genre #4 */}
-        <GridItem>
-          <NextLink href={"/language/Telugu"} passHref>
-            <Link _hover={{ textDecor: "none" }} _focus={{ boxShadow: "none" }}>
-              <MText
-                fontSize={["3xl", "3xl", "4xl"]}
-                isTruncated
-                margin={0}
-                padding={0}
-                whileHover={{
-                  scale: 1.009,
-                  color: "#AEC8CA",
-                }}
-                whileTap={{ scale: 0.96 }}
-              >
-                Telugu
-              </MText>
-            </Link>
-          </NextLink>
-        </GridItem>
-
-        {/* lang #5 */}
-        <GridItem>
-          <NextLink href={"/language/hindi"} passHref>
-            <Link _hover={{ textDecor: "none" }} _focus={{ boxShadow: "none" }}>
-              <MText
-                fontSize={["3xl", "3xl", "4xl"]}
-                isTruncated
-                margin={0}
-                padding={0}
-                whileHover={{
-                  scale: 1.009,
-                  color: "#AEC8CA",
-                }}
-                whileTap={{ scale: 0.96 }}
-              >
-                Hindi
-              </MText>
-            </Link>
-          </NextLink>
-        </GridItem>
-      </Grid>
-    </>
-  );
+	return (
+		<>
+			<Heading>Languages </Heading>
+			<SimpleGrid
+				minChildWidth={["300px", "300px", "400px"]}
+				overflowX='auto'
+				rounded='lg'
+				height={["74%", "80%", "80%"]}
+				width='full'
+				spacingX={0}
+				spacingY={9}
+				css={{
+					"&::-webkit-scrollbar": {
+						width: "7px",
+					},
+					"&::-webkit-scrollbar-track": {
+						width: "7px",
+					},
+					"&::-webkit-scrollbar-thumb": {
+						background: "pink",
+						borderRadius: "15px",
+					},
+				}}
+			>
+				<GenreBox genre='english' color='#009DAE' />
+				<GenreBox genre='tamil' color='#FFE652' />
+				<GenreBox genre='kannada' color='#396EB0' />
+				<GenreBox genre='telugu' color='#FFC4E1' />
+				<GenreBox genre='hindi' color='#AE4CCF' />
+				{/* <GenreBox genre='hip hop' color='#396EB0' /> */}
+			</SimpleGrid>
+		</>
+	);
 }
