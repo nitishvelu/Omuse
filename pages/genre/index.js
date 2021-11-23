4c64f331b62d1b948926ceb47ac99b71e346847a
nitis <nitishvelu@gmail.com>
2021-11-23 20:07:43 +0530

    formatting

--- pages/genre/index.js
+++ pages/genre/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {
-  Flex,
-  Link,
-  SimpleGrid,
-  GridItem,
-  Text,
-  IconButton,
-  Icon,
-  Image,
-  Heading,
-  Box,
+	Flex,
+	Link,
+	SimpleGrid,
+	GridItem,
+	Text,
+	IconButton,
+	Icon,
+	Image,
+	Heading,
+	Box,
 } from "@chakra-ui/react";
 import firebase from "firebase/app";
 import NextLink from "next/link";
@@ -21,54 +21,81 @@ const MText = motion(Text);
 const MImage = motion(Image);
 
 export default function Genres() {
-  return (
-    // change style for genre
-    <>
-      <Heading size="3xl">Genres </Heading>
-      <SimpleGrid
-        minChildWidth={["300px", "300px", "400px"]}
-        overflowX="auto"
-        rounded="lg"
-        height={["74%", "80%", "80%"]}
-        width="full"
-        spacingX={0}
-        spacingY={9}
-        css={{
-          "&::-webkit-scrollbar": {
-            width: "7px",
-          },
-          "&::-webkit-scrollbar-track": {
-            width: "7px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            background: "pink",
-            borderRadius: "15px",
-          },
-        }}
-      >
-        <CoolBox
-          genre="pop"
-          color="#009DAE"
-          to="genre"
-          // img='https://www.theyoungfolks.com/wp-content/uploads/2017/07/dualipa.jpg'
-        />
-        <CoolBox
-          genre="r&b"
-          color="#FFE652"
-          to="genre"
-          img="https://m.media-amazon.com/images/I/41Y9A+baBYL.jpg"
-        />
+	return (
+		// change style for genre
+		<>
+			<Heading size='3xl'>Genres </Heading>
+			<SimpleGrid
+				minChildWidth={["300px", "300px", "400px"]}
+				overflowX='auto'
+				rounded='lg'
+				height={["74%", "80%", "80%"]}
+				width='full'
+				spacingX={0}
+				spacingY={9}
+				css={{
+					"&::-webkit-scrollbar": {
+						width: "7px",
+					},
+					"&::-webkit-scrollbar-track": {
+						width: "7px",
+					},
+					"&::-webkit-scrollbar-thumb": {
+						background: "pink",
+						borderRadius: "15px",
+					},
+				}}
+			>
+				<CoolBox
+					genre='pop'
+					color='#009DAE'
+					to='genre'
+					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Flipa.jpg?alt=media&token=0eb05e9b-7f47-4f26-8bf6-68d32a42ddc4'
+				/>
+				<CoolBox
+					genre='r&b'
+					color='#FFE652'
+					to='genre'
+					img='https://m.media-amazon.com/images/I/41Y9A+baBYL.jpg'
+				/>
 
-        <CoolBox
-          genre="hip hop"
-          color="#396EB0"
-          to="genre"
-          img="https://townsquare.media/site/625/files/2015/06/kendricklamar1-630x420.jpg?w=980&q=75"
-        />
-        <CoolBox genre="soul" color="#FFC4E1" to="genre" />
-        <CoolBox genre="filmy" color="#AE4CCF" to="genre" />
-        {/* <GenreBox genre='hip hop' color='#396EB0' /> */}
-      </SimpleGrid>
-    </>
-  );
+				<CoolBox
+					genre='hip hop'
+					color='#396EB0'
+					to='genre'
+					img='https://townsquare.media/site/625/files/2015/06/kendricklamar1-630x420.jpg?w=980&q=75'
+				/>
+				<CoolBox
+					genre='melody'
+					color='#FFC4E1'
+					to='genre'
+					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fspb.jpg?alt=media&token=c68d5c29-7fe6-479b-a4b8-0e6cc8bac99b'
+				/>
+				<CoolBox
+					genre='rock'
+					color='#AE4CCF'
+					to='genre'
+					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fpanicat.jpg?alt=media&token=c6feddf4-cbec-487b-9082-4bc20998ed65'
+				/>
+				<CoolBox
+					genre='electornic'
+					color='#CAF7E3'
+					to='genre'
+					img='https://c4.wallpaperflare.com/wallpaper/144/89/99/music-marshmello-dj-marshmello-music-hd-wallpaper-thumb.jpg'
+				/>
+				<CoolBox
+					genre='jpop'
+					color='#046582'
+					to='genre'
+					img='https://c4.wallpaperflare.com/wallpaper/144/89/99/music-marshmello-dj-marshmello-music-hd-wallpaper-thumb.jpg'
+				/>
+				<CoolBox
+					genre='kpop'
+					color='#FFC898'
+					to='genre'
+					img='https://c4.wallpaperflare.com/wallpaper/144/89/99/music-marshmello-dj-marshmello-music-hd-wallpaper-thumb.jpg'
+				/>
+			</SimpleGrid>
+		</>
+	);
 }
