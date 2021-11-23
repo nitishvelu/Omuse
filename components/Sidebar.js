b43d847688b8b33e043086c511f1bb54bc170dd8
Srinivas-VJ <srinivasvj01@gmail.com>
2021-11-23 15:01:36 +0530

    artist sidebar fix

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -96,7 +96,6 @@ export default function Sidebar() {
             title="Upload"
             to="/upload"
           />
-          ;
           <NavItem
             navSize={navSize}
             icon={FiLogOut}
