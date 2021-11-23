b558f5f590e66f42aae9a098d34416800b6c87d3
Srinivas-VJ <srinivasvj01@gmail.com>
2021-11-23 12:30:05 +0530

    build check

--- pages/genre/index.js
+++ pages/genre/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {
-	Flex,
-	Link,
-	SimpleGrid,
-	GridItem,
-	Text,
-	IconButton,
-	Icon,
-	Image,
-	Heading,
-	Box,
+  Flex,
+  Link,
+  SimpleGrid,
+  GridItem,
+  Text,
+  IconButton,
+  Icon,
+  Image,
+  Heading,
+  Box,
 } from "@chakra-ui/react";
 import firebase from "firebase/app";
 import NextLink from "next/link";
@@ -21,54 +21,54 @@ const MText = motion(Text);
 const MImage = motion(Image);
 
 export default function Genres() {
-	return (
-		// change style for genre
-		<>
-			<Heading size='3xl'>Genres </Heading>
-			<SimpleGrid
-				minChildWidth={["300px", "300px", "400px"]}
-				overflowX='auto'
-				rounded='lg'
-				height={["74%", "80%", "80%"]}
-				width='full'
-				spacingX={0}
-				spacingY={9}
-				css={{
-					"&::-webkit-scrollbar": {
-						width: "7px",
-					},
-					"&::-webkit-scrollbar-track": {
-						width: "7px",
-					},
-					"&::-webkit-scrollbar-thumb": {
-						background: "pink",
-						borderRadius: "15px",
-					},
-				}}
-			>
-				<CoolBox
-					genre='pop'
-					color='#009DAE'
-					to='genre'
-					img='https://www.theyoungfolks.com/wp-content/uploads/2017/07/dualipa.jpg'
-				/>
-				<CoolBox
-					genre='r&b'
-					color='#FFE652'
-					to='genre'
-					img='https://m.media-amazon.com/images/I/41Y9A+baBYL.jpg'
-				/>
+  return (
+    // change style for genre
+    <>
+      <Heading size="3xl">Genres </Heading>
+      <SimpleGrid
+        minChildWidth={["300px", "300px", "400px"]}
+        overflowX="auto"
+        rounded="lg"
+        height={["74%", "80%", "80%"]}
+        width="full"
+        spacingX={0}
+        spacingY={9}
+        css={{
+          "&::-webkit-scrollbar": {
+            width: "7px",
+          },
+          "&::-webkit-scrollbar-track": {
+            width: "7px",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "pink",
+            borderRadius: "15px",
+          },
+        }}
+      >
+        <CoolBox
+          genre="pop"
+          color="#009DAE"
+          to="genre"
+          // img='https://www.theyoungfolks.com/wp-content/uploads/2017/07/dualipa.jpg'
+        />
+        <CoolBox
+          genre="r&b"
+          color="#FFE652"
+          to="genre"
+          img="https://m.media-amazon.com/images/I/41Y9A+baBYL.jpg"
+        />
 
-				<CoolBox
-					genre='hip hop'
-					color='#396EB0'
-					to='genre'
-					img='https://townsquare.media/site/625/files/2015/06/kendricklamar1-630x420.jpg?w=980&q=75'
-				/>
-				<CoolBox genre='soul' color='#FFC4E1' to='genre' />
-				<CoolBox genre='filmy' color='#AE4CCF' to='genre' />
-				{/* <GenreBox genre='hip hop' color='#396EB0' /> */}
-			</SimpleGrid>
-		</>
-	);
+        <CoolBox
+          genre="hip hop"
+          color="#396EB0"
+          to="genre"
+          img="https://townsquare.media/site/625/files/2015/06/kendricklamar1-630x420.jpg?w=980&q=75"
+        />
+        <CoolBox genre="soul" color="#FFC4E1" to="genre" />
+        <CoolBox genre="filmy" color="#AE4CCF" to="genre" />
+        {/* <GenreBox genre='hip hop' color='#396EB0' /> */}
+      </SimpleGrid>
+    </>
+  );
 }
