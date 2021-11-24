e4ae5a7d6a073b90b810409f8b2a1cb6ac37af8a
nitis <nitishvelu@gmail.com>
2021-11-24 23:14:31 +0530

    custom error pages and removed ugly loading text

--- src/hook/route.js
+++ src/hook/route.js
@@ -1,43 +1,42 @@
-import { useRouter } from 'next/router';
-import React from 'react'
-import useAuth from './auth'
+import { useRouter } from "next/router";
+import React from "react";
+import useAuth from "./auth";
 import cookie from "js-cookie";
 
+export function withPublic(Component) {
+	return function WithPublic(props) {
+		const auth = useAuth();
+		const router = useRouter();
 
-export  function withPublic(Component) {
-    return function WithPublic(props){
-        const auth=useAuth();
-        const router=useRouter();
-        
-        if(auth.user){
-            router.replace("/app");
-            return<h1>loading......</h1>
-        }
-        return<Component auth={auth}{...props}/>
-    }
+		if (auth.user) {
+			router.replace("/app");
+			return <h1></h1>;
+		}
+		return <Component auth={auth} {...props} />;
+	};
 }
 
-export  function withProtected(Component) {
-    return function WithProtected(props){
-        const auth=useAuth();
-        const router=useRouter();
-        
-        if(!auth.user){
-            router.replace("/");
-            return<h1>loading......</h1>
-        }
-        return<Component auth={auth}{...props}/>
-    }
+export function withProtected(Component) {
+	return function WithProtected(props) {
+		const auth = useAuth();
+		const router = useRouter();
+
+		if (!auth.user) {
+			router.replace("/");
+			return <h1></h1>;
+		}
+		return <Component auth={auth} {...props} />;
+	};
 }
-export  function withArtist(Component) {
-    return function WithArtist(props){
-        const auth=useAuth();
-        const router=useRouter();
-        
-        if(!auth.user || cookie.get("typeOfUser")!="Artist"){
-            router.replace("/");
-            return<h1>loading......</h1>
-        }
-        return<Component auth={auth}{...props}/>
-    }
+export function withArtist(Component) {
+	return function WithArtist(props) {
+		const auth = useAuth();
+		const router = useRouter();
+
+		if (!auth.user || cookie.get("typeOfUser") != "Artist") {
+			router.replace("/");
+			return <h1></h1>;
+		}
+		return <Component auth={auth} {...props} />;
+	};
 }
