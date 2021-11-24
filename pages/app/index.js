b972f3930e03921e2e9b60d7a01c5e347b4a04ba
nitis <nitishvelu@gmail.com>
2021-11-24 21:08:16 +0530

    colors dashboard

--- pages/app/index.js
+++ pages/app/index.js
@@ -97,43 +97,6 @@ export default function Language() {
 					to='app/artist'
 					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/p.jpeg?alt=media&token=e90ea1a3-c8f0-4186-b653-cb495281eca4'
 				/>
-
-				{/* <CoolBox
-          genre="kannada"
-          color="#396EB0"
-          to="language"
-          img="https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fsarja.png?alt=media&token=6c2661bd-ecf9-484f-ad89-c933e3403eca"
-        />
-        <CoolBox
-          genre="telugu"
-          color="#FFC4E1"
-          to="language"
-          img="https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fdsp.jpeg?alt=media&token=fe148013-cc98-41e2-a790-6a06657bc73e"
-        />
-        <CoolBox
-          genre="hindi"
-          color="#AE4CCF"
-          to="language"
-          img="https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Farijit.jpg?alt=media&token=df55fe2a-4ba8-412e-96c7-e0694812e9c0"
-        />
-        <CoolBox
-          genre="electornic"
-          color="#CAF7E3"
-          to="genre"
-          img="https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fmello.jpg?alt=media&token=977c10d7-8c64-49d7-8f7a-e6657ad9c46a"
-        />
-        <CoolBox
-          genre="jpop"
-          color="#046582"
-          to="genre"
-          img="https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fmello.jpg?alt=media&token=977c10d7-8c64-49d7-8f7a-e6657ad9c46a"
-        />
-        <CoolBox
-          genre="kpop"
-          color="#FFC898"
-          to="genre"
-          img="https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fmello.jpg?alt=media&token=977c10d7-8c64-49d7-8f7a-e6657ad9c46a" 
-        />   */}
 			</SimpleGrid>
 		</>
 	);
