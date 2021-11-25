897fdb8900984cbcb9444f016f8e584130fdf660
nitis <nitishvelu@gmail.com>
2021-11-25 08:33:26 +0530

    clean logout page

--- pages/logout.js
+++ pages/logout.js
@@ -9,34 +9,24 @@ function Logout({ auth }) {
 	const { logout } = auth;
 	return (
 		<div>
-			<Flex
-				justifyContent='center'
-				alignItems='center'
-				h='85vh'
-				width='95%'
-				flexDirection='column'
+			<MotionButton
+				drag='x'
+				dragConstraints={{ left: -0, right: 0 }}
+				_focus={{ boxShadow: "none" }}
+				whileHover={{ scale: 1.1 }}
+				whileTap={{ scale: 0.9 }}
+				animate={{ rotate: 360 }}
+				transition={{ duration: 0.1 }}
+				onClick={() => {
+					logout();
+					cookie.remove("uid");
+					cookie.remove("name");
+					cookie.remove("typeOfUser");
+				}}
 			>
-				<Heading size='4xl'>Good Bye!!</Heading>
-				<MotionButton
-					drag='x'
-					dragConstraints={{ left: -0, right: 0 }}
-					_focus={{ boxShadow: "none" }}
-					whileHover={{ scale: 1.1 }}
-					whileTap={{ scale: 0.9 }}
-					animate={{ rotate: 360 }}
-					transition={{ duration: 0.1 }}
-					onClick={() => {
-						logout();
-						cookie.remove("uid");
-						cookie.remove("name");
-						cookie.remove("typeOfUser");
-					}}
-				>
-					Logout
-				</MotionButton>
-			</Flex>
+				Logout
+			</MotionButton>
 		</div>
 	);
 }
 export default withProtected(Logout);
-//
