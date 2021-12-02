07bc0cd495881f8e9b45a8d233ade5a67fa35134
nitis <nitishvelu@gmail.com>
2021-12-02 05:48:42 +0530

    image caching with sharp and turning off lazy loading

--- components/ChakraNextImage.js
+++ components/ChakraNextImage.js
@@ -5,7 +5,14 @@ export const ChakraNextImage = (props) => {
 	const { src, alt, ...rest } = props;
 	return (
 		<Box position='relative' {...rest}>
-			<NextImage objectFit='cover' layout='fill' src={src} alt={alt} />
+			<NextImage
+				objectFit='cover'
+				layout='fill'
+				src={src}
+				alt={alt}
+				priority
+				loading='eager'
+			/>
 		</Box>
 	);
 };
--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
 	reactStrictMode: true,
 	images: {
 		domains: ["firebasestorage.googleapis.com"],
+		minimumCacheTTL: 31536000,
 	},
 };
