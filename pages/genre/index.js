f278f68ab32f173e114badc1f0a8b4f9edef0c0b
nitis <nitishvelu@gmail.com>
2021-12-05 15:34:36 +0530

    minified images with size 300x300

--- pages/genre/index.js
+++ pages/genre/index.js
@@ -52,14 +52,14 @@ function Genres() {
 					name='Pop'
 					color='#009DAE'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Flipa.jpg?alt=media&token=0eb05e9b-7f47-4f26-8bf6-68d32a42ddc4'
+					img='/images/lipa.jpg'
 				/>
 				<CoolBox
 					id='r&b'
 					name='Rhythm And Blues'
 					color='#FFE652'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fabel.jpg?alt=media&token=21d24aec-2705-4995-b7de-24a185a12881'
+					img='/images/abel.jpg'
 				/>
 
 				<CoolBox
@@ -67,42 +67,42 @@ function Genres() {
 					name='Hip-Hop'
 					color='#396EB0'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Flamar.jpeg?alt=media&token=730f7206-1282-4b91-b46c-facb818a29de'
+					img='/images/lamar.jpg'
 				/>
 				<CoolBox
 					id='melody'
 					name='Melody'
 					color='#FFC4E1'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fspb.jpg?alt=media&token=c68d5c29-7fe6-479b-a4b8-0e6cc8bac99b'
+					img='/images/spb.jpg'
 				/>
 				<CoolBox
 					id='rock'
 					name='rock'
 					color='#AE4CCF'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fpanicat.jpg?alt=media&token=c6feddf4-cbec-487b-9082-4bc20998ed65'
+					img='/images/panicat.jpg'
 				/>
 				<CoolBox
 					id='electronic'
 					name='Electronic'
 					color='#CAF7E3'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fmello.jpg?alt=media&token=977c10d7-8c64-49d7-8f7a-e6657ad9c46a'
+					img='/images/mello.jpg'
 				/>
 				<CoolBox
 					id='jpop'
 					name='Jpop'
 					color='#046582'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fjapane.jpg?alt=media&token=9a741abe-7ca7-49cd-8637-0000c61de9a8'
+					img='/images/japane.jpg'
 				/>
 				<CoolBox
 					id='kpop'
 					name='K PoP'
 					color='#FFC898'
 					to='genre'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fjimin.jpg?alt=media&token=6c00cf1f-735d-434f-a47e-751dff574873'
+					img='/images/jimin.jpg'
 				/>
 			</SimpleGrid>
 		</>
