ed85680d6bb8199f9f079cd0e10f1a951a5c3ddf
nitis <nitishvelu@gmail.com>
2021-12-05 15:42:19 +0530

    storing images on vercel server instead of firebase

--- pages/language/index.js
+++ pages/language/index.js
@@ -46,7 +46,7 @@ function Language() {
 					name='English'
 					color='#009DAE'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fderulo.jpeg?alt=media&token=05666cb7-fe7e-49eb-b280-1386d7a95a48'
+					img='/images/derulo.jpg'
 				/>
 				{/* gjgj */}
 				<CoolBox
@@ -54,49 +54,49 @@ function Language() {
 					name='Tamil'
 					color='#FFE652'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Frahaman.jpeg?alt=media&token=252ada29-ef69-45cf-aab7-3f080618fee2'
+					img='/images/rahman.jpg'
 				/>
 				<CoolBox
 					id='kannada'
 					name='Kannada'
 					color='#396EB0'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fsarja.png?alt=media&token=6c2661bd-ecf9-484f-ad89-c933e3403eca'
+					img='/images/sarja.jpg'
 				/>
 				<CoolBox
 					id='telugu'
 					name='Telugu'
 					color='#FFC4E1'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fdsp.jpeg?alt=media&token=fe148013-cc98-41e2-a790-6a06657bc73e'
+					img='/images/dsp.jpg'
 				/>
 				<CoolBox
 					id='hindi'
 					name='Hindi'
 					color='#AE4CCF'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Farijit.jpg?alt=media&token=df55fe2a-4ba8-412e-96c7-e0694812e9c0'
+					img='/images/arijit.jpg'
 				/>
 				<CoolBox
 					id='malayalam'
 					name='Malayalam'
 					color='#CAF7E3'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fvijayyesu.jpg?alt=media&token=109b0bc8-5339-4260-bec1-adf7ae3db75b'
+					img='/images/vijay.jpg'
 				/>
 				<CoolBox
 					id='japanese'
 					name='Japanese'
 					color='#046582'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fkumi.jpg?alt=media&token=6622b6f6-a9a4-4e83-abe3-63488459b86c'
+					img='/images/kumi.jpg'
 				/>
 				<CoolBox
 					id='korean'
 					name='Korean'
 					color='#FFC898'
 					to='language'
-					img='https://firebasestorage.googleapis.com/v0/b/omuse-ac842.appspot.com/o/image_assets%2Fsuga.png?alt=media&token=c87d647d-22e2-4ab8-b84f-9df1b09cbef6'
+					img='/images/suga.jpg'
 				/>
 			</SimpleGrid>
 		</>
