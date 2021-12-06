3cbae6a40e5c9d83035f773a4874fa313d3c06a6
nitis <nitishvelu@gmail.com>
2021-12-06 15:11:36 +0530

    stable point trying to check something

--- src/service/Chakra.js
+++ src/service/Chakra.js
@@ -1,32 +1,34 @@
 // e.g. src/Chakra.js
 // a) import `ChakraProvider` component as well as the storageManagers
 import {
-    ChakraProvider,
-    cookieStorageManager,
-    localStorageManager,
-  } from "@chakra-ui/react"
-  
-  export function Chakra({ cookies, children }) {
-    // b) Pass `colorModeManager` prop
-    const colorModeManager =
-      typeof cookies === "string"
-        ? cookieStorageManager(cookies)
-        : localStorageManager
-  
-    return (
-      <ChakraProvider colorModeManager={colorModeManager}>
-        {children}
-      </ChakraProvider>
-    )
-  }
-  
-  // also export a reusable function getServerSideProps
-  export function getServerSideProps({ req }) {
-    return {
-      props: {
-        // first time users will not have any cookies and you may not return
-        // undefined here, hence ?? is necessary
-        cookies: req.headers.cookie ?? "",
-      },
-    }
-  }
+	ChakraProvider,
+	cookieStorageManager,
+	localStorageManager,
+} from "@chakra-ui/react";
+
+import { theme } from "../../theme";
+
+export function Chakra({ cookies, children }) {
+	// b) Pass `colorModeManager` prop
+	const colorModeManager =
+		typeof cookies === "string"
+			? cookieStorageManager(cookies)
+			: localStorageManager;
+
+	return (
+		<ChakraProvider theme={theme} colorModeManager={colorModeManager}>
+			{children}
+		</ChakraProvider>
+	);
+}
+
+// also export a reusable function getServerSideProps
+export function getServerSideProps({ req }) {
+	return {
+		props: {
+			// first time users will not have any cookies and you may not return
+			// undefined here, hence ?? is necessary
+			cookies: req.headers.cookie ?? "",
+		},
+	};
+}
--- theme.js
+++ theme.js
@@ -1,15 +1,37 @@
 // theme.js
 
 // 1. import `extendTheme` function
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 // 2. Add your color mode config
-const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: true,
-}
+// const config = {
+// 	initialColorMode: "dark",
+// 	useSystemColorMode: true,
+// };
+// const styles = {
+// 	global: (props) => ({
+// 		body: {
+// 			color: mode("pink.800", "red.900")(props),
+// 			bg: mode("blue.100", "#141214")(props),
+// 		},
+// 	}),
+// };
 
-// 3. extend the theme
-const theme = extendTheme({ config })
+// // 3. extend the theme
+// const theme = extendTheme({ config, styles });
 
-export default theme
+const theme = extendTheme({
+	styles: {
+		global: (props) => ({
+			body: {
+				fontFamily: "body",
+				color: mode("gray.800", "whiteAlpha.900")(props),
+				bg: mode("pink", "gray.800")(props),
+				lineHeight: "base",
+			},
+		}),
+	},
+});
+
+export default theme;
