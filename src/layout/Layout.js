7c761bce81a8550b4884ff1d64a69a66265209e0
nitis <nitishvelu@gmail.com>
2021-12-08 14:32:48 +0530

    music player in mobile color mode compatible

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,20 +4,12 @@ import { HStack, VStack, Grid, GridItem, Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
-import {
-	FiChevronLeft,
-	FiChevronRight,
-	FiFastForward,
-	FiPause,
-	FiPauseCircle,
-	FiPlay,
-	FiPlayCircle,
-	FiRepeat,
-} from "react-icons/fi";
+import { FiPauseCircle, FiPlayCircle } from "react-icons/fi";
 import { AiOutlineDoubleRight, AiOutlineDoubleLeft } from "react-icons/ai";
-
+import { useColorModeValue } from "@chakra-ui/color-mode";
 export default function Layout({ children }) {
 	const router = useRouter();
+	const value = useColorModeValue("green.50", "gray.700");
 	if (router.pathname != "/") {
 		return (
 			<>
@@ -46,20 +38,19 @@ export default function Layout({ children }) {
 							// gap={1}
 							// rounded='md'
 						>
-							<GridItem rowSpan={32} colSpan={30}>
+							<GridItem rowSpan={33} colSpan={30}>
 								{children}
 							</GridItem>
-							<GridItem rowSpan={8} colSpan={30}>
+							<GridItem rowSpan={7} colSpan={30}>
 								<Box id='musicPlayer'>
 									<Box
 										as={AudioPlayer}
 										width={["75%", "80%", "90%"]}
 										height={["12%", "12%", "auto"]}
 										position='fixed'
-										bottom={[0.1, 1, 3]}
+										bottom={[1, 1, 3]}
 										opacity={1}
-										backgroundColor='gray.700'
-										// b='grey'
+										backgroundColor={value}
 										customIcons={{
 											play: <FiPlayCircle />,
 											pause: <FiPauseCircle />,
