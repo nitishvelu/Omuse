22b4635ba4b6f0dfbbc002863ba1e19973c87146
nitis <nitishvelu@gmail.com>
2021-12-08 19:01:27 +0530

    fixing margins

--- components/CoolGrid.js
+++ components/CoolGrid.js
@@ -3,7 +3,7 @@ import { SimpleGrid, Heading } from "@chakra-ui/layout";
 function CoolGrid(props) {
 	return (
 		<>
-			<Heading size='3xl' mb={[2, 0, 0]} mt={[2, 0, 0]}>
+			<Heading size='3xl' mb={[1, 0, 0]} mt={[2, 0, 0]}>
 				{props.title.replace(/(?:^|\s|["'([{])+\S/g, (match) =>
 					match.toUpperCase()
 				)}
